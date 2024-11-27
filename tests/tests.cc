da127f0aebdcb63b2c4d3efa797f839af1346a1d
Sergio Gonzalez Barraza <114702763+SergioGzzBrz@users.noreply.github.com>
2024-11-26 19:01:28 -0600

    Add unit testing

---
+++ tests/tests.cc
@@ -0,0 +1,23 @@
+#include "parte_1.h"
+#include "parte_2.h"
+#include "parte_3.h"
+#include "parte_4.h"
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("Testing with DSU") {
+    // Testing DSU
+    dsu_union(1, 2);
+    dsu_union(4, 5);
+    dsu_union(2, 5);
+    REQUIRE(get(1) == get(5));
+    REQUIRE(get(1) == get(4));
+    REQUIRE(get(2) == get(4));
+}
+
+TEST_CASE("euclidean_distance") {
+    pair<int, int> a = make_pair(0, 2);
+    pair<int, int> b = make_pair(2, 3);
+    double res = sqrt(5);
+    double eplison = 1e-6;
+    REQUIRE(abs(euclidean_distance(a, b) - res) <= eplison);
+}
